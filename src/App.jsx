3150fcbf393bbb218012c3a11b0857f45ba598b6
Maria Eduarda <maria.mirandadev@gmail.com>
2024-05-12 18:22:41 -0300

    repositorio tres

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ConversorDeMoedas from './components/Conversor/Conversor'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <ConversorDeMoedas/>
+    </div>
+  )
+}
